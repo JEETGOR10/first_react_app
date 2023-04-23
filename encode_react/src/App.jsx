60369ceee34b859133fb6239a258b34a7a9445b5
unknown <gorjeet1333@gmail.com>
2023-04-23 10:14:58 +0530

    hi

---
+++ encode_react/src/App.jsx
@@ -0,0 +1,14 @@
+// write name with first capital 
+import Button from "./Button.jsx";
+function App(){
+    return(
+        <div>
+            <Button/>
+            <h1>React is awsm</h1>
+            <h2>i am loving it</h2>
+            <h6>you must try it out</h6>
+        </div>
+    )
+}
+
+export default App;
---
+++ encode_react/src/Button.jsx
@@ -0,0 +1,7 @@
+function Button(){
+    return(
+        <button>I am a BUTTON</button>
+    )
+}
+
+export default Button;
